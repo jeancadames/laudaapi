11c23330014f54491dee170d60189e95227f6d14
luisjadames <luisjadames@gmail.com>
2026-08-23 02:51:51 +0000

    update: 2026-08-23 02:51

--- resources/js/pages/Admin/DiagnosisRequests/Show.vue
+++ resources/js/pages/Admin/DiagnosisRequests/Show.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { Head, Link, router, useForm } from '@inertiajs/vue3';
 import {
     ArrowLeft,
